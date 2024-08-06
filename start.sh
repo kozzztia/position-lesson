5ebfe8109b59e8dd2280d6b861c66f27752ce3bd
kozzztia <kozzztia@gmail.com>
2024-08-06 12:21:48 +0300

    initial commit

---
+++ start.sh
@@ -0,0 +1,21 @@
+# if [ -z "$1" ]
+# then
+#   echo "Пожалуйста, укажите сообщение коммита"
+#   exit 1
+# fi
+
+git init
+git add .
+git commit -m "initial commit"
+git branch -M main
+git remote add origin git@github.com:kozzztia/position-lesson.git
+git push -u origin main
+
+# git add .
+# git commit -m "$1"
+# git push
+
+echo "Всё готово!"
+
+# chmod +x start.sh
+# ./start.sh "Ваше сообщение коммита здесь"
